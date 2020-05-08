b56e12b179db71e94764ed014850b0ce28973eea
James King <ripixel@gmail.com>
2020-05-08 01:33:40 +0100

    feat: Basic subpages implemented

--- scripts/generatePages.ts
+++ scripts/generatePages.ts
@@ -34,15 +34,22 @@ const pages = findInDir("./pages", ".html");
 pages.forEach((page) => {
   console.log(`Processing ${page}`);
   let pageContents = fs.readFileSync(page, "utf8");
+  let pageName = page.replace("pages/", "").replace(".html", "");
+  if (pageName === "index") {
+    pageName = "home";
+  }
 
   templates.forEach((template) => {
+    const templateContents = fs
+      .readFileSync(template, "utf8")
+      .replace(new RegExp(`{page}`, "g"), pageName);
+
+    templateContents;
     pageContents = pageContents.replace(
       new RegExp(`{${template.replace("templates/", "")}}`, "g"),
-      fs.readFileSync(template, "utf8")
+      templateContents
     );
   });
-
-  console.log(`Finished processing`);
 
   fs.writeFileSync(page.replace("pages/", "public/"), pageContents, "utf8");
 });
