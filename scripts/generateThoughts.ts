ccfb5ea2482782548bc4e143bc100c8d5e61a5b6
James King <ripixel@gmail.com>
2020-05-10 22:20:55 +0100

    feat!: Auto-gen thoughts page proper from articles

--- scripts/generateThoughts.ts
+++ scripts/generateThoughts.ts
@@ -30,17 +30,22 @@ const articles = findInDir("./thoughts/articles", ".md");
 
 const mdConverter = new showdown.Converter();
 
-const articlesGenerated = [];
+const articlesGenerated: Array<{
+  title: string;
+  link: string;
+  body: string;
+  date: string;
+}> = [];
 
 articles.forEach((article) => {
   const articleWithoutFolder = article
     .replace("thoughts/articles/", "")
     .replace(".md", ".html");
-  let [datestring, title] = articleWithoutFolder
+  let [datestring, titleWithDash] = articleWithoutFolder
     .replace(".html", "")
     .split("_");
   const date = dateFormat(new Date(datestring), "do LLL, u");
-  title = title.replace(/-/g, " ");
+  const title = titleWithDash.replace(/-/g, " ");
   const body = mdConverter.makeHtml(fs.readFileSync(article, "utf8"));
 
   const articleContents = thoughtsPageTemplateContents
@@ -55,13 +60,31 @@ articles.forEach((article) => {
     "utf8"
   );
 
+  const splitBody = body.split("</p>");
+
   articlesGenerated.push({
+    link: `${datestring}_${titleWithDash}`,
     title,
     date,
-    body,
+    body: `${splitBody[0]}</p>${splitBody[1]}</p>`,
   });
 });
 
 console.log(`Generated ${articlesGenerated.length} articles`);
+
+console.log("Updating thoughts page proper");
+let thoughtsPageContents = fs.readFileSync("./public/thoughts.html", "utf8");
+
+for (let i = 0; i < Math.min(2, articlesGenerated.length); i++) {
+  thoughtsPageContents = thoughtsPageContents
+    .replace(`{title${i}}`, articlesGenerated[i].title)
+    .replace(`{date${i}}`, articlesGenerated[i].date)
+    .replace(`{body${i}}`, articlesGenerated[i].body)
+    .replace(`{link${i}}`, articlesGenerated[i].link);
+}
+
+console.log("Updated thoughts page");
+
+fs.writeFileSync("./public/thoughts.html", thoughtsPageContents, "utf8");
 
 console.log("/// Finished generation of thoughts");
